c964e557a7ba61119d57753c99975b2ae385af24
Youn-Hee Han <yh21.han@gmail.com>
2022-10-30 17:39:32 +0900

    ...

--- offline_class/e_dqn_atari_pong/dqn_train_pong.py
+++ offline_class/e_dqn_atari_pong/dqn_train_pong.py
@@ -274,7 +274,7 @@ def main():
 
     config = {
         "env_name": ENV_NAME,                       # 환경의 이름
-        "max_num_episodes": 20_000,                 # 훈련을 위한 최대 에피소드 횟수
+        "max_num_episodes": 5_000,                 # 훈련을 위한 최대 에피소드 횟수
         "batch_size": 32,                           # 훈련시 배치에서 한번에 가져오는 랜덤 배치 사이즈
         "learning_rate": 0.0001,                    # 학습율
         "gamma": 0.99,                              # 감가율
